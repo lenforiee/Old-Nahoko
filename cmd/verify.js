acdf746bf843eaff274ec7662168ed0040341651
root <root@cloud5849252.nitrado.cloud>
2020-06-11 23:27:35 +0200

    major bug fixed

--- cmd/verify.js
+++ cmd/verify.js
@@ -3,8 +3,8 @@ var colors = require('colors/safe');
 
 module.exports = function(client) {
     client.on('message', async message => {
-      if (message.channel.id === '714938918209978481') {
-        if (message.content.startsWith(config.bot.prefix + "verify")) {
+      if (message.content.startsWith(config.bot.prefix + "verify")) {
+        if (message.channel.id === config.bot.verify) {
             try {
               var role = message.guild.roles.find(role => role.name === "Member");
               message.member.addRole(role);
@@ -17,9 +17,9 @@ module.exports = function(client) {
               console.log(error)
               message.channel.send("This shouldn't happened :/");
             }
+          } else {
+            message.delete();
           }
-        } else {
-          message.delete();
         }
     })
 }
